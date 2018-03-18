55d1513cbcfea56e2f98ad0fe31edb0e3986e6d0
Ngadou Sylvestre <mathalpha26@gmail.com>
2018-03-18 11:37:47 +0100

    Simple form (skeleton)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function changeLang() {
+  var SL = document.getElementById('sL').value;
+  var TL = document.getElementById('tL').value;
+  console.log("Translating from "+SL+" to "+TL+"\n");
+  //document.getElementById('sll').innerHTML = SL;
+  //document.getElementById('tll').innerHTML = TL;
+}
