efa08ae80bbfd6c64c360b7cbecf43d10c5c93b3
math-alpha <mathalpha26@gmail.com>
2019-07-12 11:21:06 +0100

    first version

--- server/server.js
+++ server/server.js
@@ -3,25 +3,30 @@ var bodyParser     =        require("body-parser");
 var fs             =        require("fs");
 var app            =        express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/',function(req,res){
     res.sendfile("server/index.html");
 });
 app.post('/add',function(req,res){
-    var med=req.body._med;
-    var medT=req.body._medt;
-    var fra=req.body._fra;
-    var fraT=req.body._frat;
-    var l = "\<e\>\<p\>\<l\>la\<\b\/>nstə<b/>ba\ʼ<s n=\"n\"/><s n=\"cl3\"/></l>";
-    var r = "\<r\>bateau<s n=\"n\"/><s n=\"m\"/><s n=\"sg\"/></r></p></e>";
+    var med = req.body._med;
+    var medT = req.body._medt;
+    var fra = req.body._fra;
+    var fraT = req.body._frat;
+    var l = "\<e\>\<p\>\<l\>"+med+"<s n=\"n\"/></l>";
+    var r = "\<r\>"+fra+" \<s n=\"n\"/></r></p></e>";
 
-    fs.appendFile('byv-fra.xml', "\n\nMed text = "+med+"----- Tag "+medT+"\nfra text = "+fra+"---- Tag "+fraT+" |\n", (err) => {
+    var bidixText = l + r + "\n";
+    var monodixText = med + ":"+med +" "+medT+ " # ; !\""+fra+"\"\n"
+
+    console.log(l + " \n "+r+"\nmedt "+medT+"\n frat "+fraT);
+
+    fs.appendFile('byv-fra.xml', bidixText, (err) => {
         if (err) throw err;
         console.log('The "data to append" was appended to file!');
     })
-    fs.appendFile('byv.txt', "\n\nMed text = "+med+"----- Tag "+medT+"\nfra text = "+fra+"---- Tag "+fraT+" |\n", (err) => {
+    fs.appendFile('byv.txt', monodixText, (err) => {
         if (err) throw err;
         console.log('The "data to append" was appended to file!');
     })
